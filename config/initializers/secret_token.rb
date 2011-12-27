d5a1491e6a2726e16a5d398d4415d8b6fa9ad181
hadoop <hadoop@hongkim-desktop.(none)>
2011-12-26 23:45:57 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = '745ca568e2f39f34e90b24c2215219a9f3dbf83b6faea1a57bb08ecda7bec92d294d526313043cc64199f397dacc69256fbd3a58a02e90cbd86fbdef91ed1124'
